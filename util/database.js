69e04b41e5fd3616c20984ce7d53a34241e05d0c
Pushkar Sabe <pushkarsabe@gmail.com>
2025-09-22 23:32:51 +0530

    changes made

--- util/database.js
+++ util/database.js
@@ -2,21 +2,27 @@ const { Sequelize } = require('sequelize');
 
 let sequelize;
 
-if (process.env.DATABASE_URL) {
-    // This block will be used by Render
-    sequelize = new Sequelize(process.env.DATABASE_URL, {
-        dialect: 'postgres',
-        protocol: 'postgres',
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false // Necessary for Render's Postgres
+if (process.env.RENDER) {
+    console.log("Connecting to Aiven MySQL database on Render...");
+    sequelize = new Sequelize(
+        process.env.DB_DATABASE,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            dialect: 'mysql',
+            dialectOptions: {
+                ssl: {
+                    require: true,
+                    rejectUnauthorized: false,
+                    ca: process.env.DB_CERT,
+                }
             }
         }
-    });
-}
-else {
-
+    );
+} else {
+    console.log("Connecting to local MySQL database...");
     sequelize = new Sequelize(
         process.env.DB_NAME,
         process.env.DB_USER,
@@ -27,6 +33,5 @@ else {
         }
     );
 }
-
 
 module.exports = sequelize;
